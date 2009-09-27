dd8fe2ffee6ac6d9e593bbd2c3c57501e99caaa4
Serge Vakulenko <serge.vakulenko@gmail.com>
2009-09-27 15:28:33 +0000

    Реализована загрузка и выгрузка файлов командами load и dump. В нескольких местах добавлено обращение к пультовым регистрам 1-7 вместо памяти. Добавлен пример запуска на SIMH пультовой программы 6: тест уножения-деления. Тест идёт успешно.

--- simh/besm6_defs.h
+++ simh/besm6_defs.h
@@ -37,8 +37,11 @@ enum {
 /*
  * Разряды машинного слова.
  */
+#define BITS12		07777			/* биты 12..1 */
 #define BITS15		077777			/* биты 15..1 */
 #define BITS24		077777777		/* биты 24..1 */
+#define BIT19		001000000		/* бит 19 - расширение адреса короткой команды */
+#define BIT20		002000000		/* бит 20 - признак длинной команды */
 #define WORD		07777777777777777LL	/* биты 48..1 */
 #define MANTISSA	00037777777777777LL	/* биты 41..1 */
 #define SIGN		00020000000000000LL	/* 41-й бит-знак */
@@ -66,7 +69,7 @@ enum {
 
 extern uint32 sim_brk_types, sim_brk_dflt, sim_brk_summ; /* breakpoint info */
 extern int32 sim_interval, sim_step;
-extern FILE *sim_deb;
+extern FILE *sim_deb, *sim_log;
 
 extern UNIT cpu_unit;
 extern t_value memory [MEMSIZE];
@@ -99,6 +102,73 @@ extern jmp_buf cpu_halt;
 #define dis_round               (RAU & RAU_ROUND_DISABLE)
 #define dis_exc                 (RAU & RAU_OVF_DISABLE)
 
+/*
+ * Кириллица Unicode.
+ */
+#define CYRILLIC_CAPITAL_LETTER_A		0x0410
+#define CYRILLIC_CAPITAL_LETTER_BE		0x0411
+#define CYRILLIC_CAPITAL_LETTER_VE		0x0412
+#define CYRILLIC_CAPITAL_LETTER_GHE		0x0413
+#define CYRILLIC_CAPITAL_LETTER_DE		0x0414
+#define CYRILLIC_CAPITAL_LETTER_IE		0x0415
+#define CYRILLIC_CAPITAL_LETTER_ZHE		0x0416
+#define CYRILLIC_CAPITAL_LETTER_ZE		0x0417
+#define CYRILLIC_CAPITAL_LETTER_I		0x0418
+#define CYRILLIC_CAPITAL_LETTER_SHORT_I		0x0419
+#define CYRILLIC_CAPITAL_LETTER_KA		0x041a
+#define CYRILLIC_CAPITAL_LETTER_EL		0x041b
+#define CYRILLIC_CAPITAL_LETTER_EM		0x041c
+#define CYRILLIC_CAPITAL_LETTER_EN		0x041d
+#define CYRILLIC_CAPITAL_LETTER_O		0x041e
+#define CYRILLIC_CAPITAL_LETTER_PE		0x041f
+#define CYRILLIC_CAPITAL_LETTER_ER		0x0420
+#define CYRILLIC_CAPITAL_LETTER_ES		0x0421
+#define CYRILLIC_CAPITAL_LETTER_TE		0x0422
+#define CYRILLIC_CAPITAL_LETTER_U		0x0423
+#define CYRILLIC_CAPITAL_LETTER_EF		0x0424
+#define CYRILLIC_CAPITAL_LETTER_HA		0x0425
+#define CYRILLIC_CAPITAL_LETTER_TSE		0x0426
+#define CYRILLIC_CAPITAL_LETTER_CHE		0x0427
+#define CYRILLIC_CAPITAL_LETTER_SHA		0x0428
+#define CYRILLIC_CAPITAL_LETTER_SHCHA		0x0429
+#define CYRILLIC_CAPITAL_LETTER_HARD_SIGN	0x042a
+#define CYRILLIC_CAPITAL_LETTER_YERU		0x042b
+#define CYRILLIC_CAPITAL_LETTER_SOFT_SIGN	0x042c
+#define CYRILLIC_CAPITAL_LETTER_E		0x042d
+#define CYRILLIC_CAPITAL_LETTER_YU		0x042e
+#define CYRILLIC_CAPITAL_LETTER_YA		0x042f
+#define CYRILLIC_SMALL_LETTER_A			0x0430
+#define CYRILLIC_SMALL_LETTER_BE		0x0431
+#define CYRILLIC_SMALL_LETTER_VE		0x0432
+#define CYRILLIC_SMALL_LETTER_GHE		0x0433
+#define CYRILLIC_SMALL_LETTER_DE		0x0434
+#define CYRILLIC_SMALL_LETTER_IE		0x0435
+#define CYRILLIC_SMALL_LETTER_ZHE		0x0436
+#define CYRILLIC_SMALL_LETTER_ZE		0x0437
+#define CYRILLIC_SMALL_LETTER_I			0x0438
+#define CYRILLIC_SMALL_LETTER_SHORT_I		0x0439
+#define CYRILLIC_SMALL_LETTER_KA		0x043a
+#define CYRILLIC_SMALL_LETTER_EL		0x043b
+#define CYRILLIC_SMALL_LETTER_EM		0x043c
+#define CYRILLIC_SMALL_LETTER_EN		0x043d
+#define CYRILLIC_SMALL_LETTER_O			0x043e
+#define CYRILLIC_SMALL_LETTER_PE		0x043f
+#define CYRILLIC_SMALL_LETTER_ER		0x0440
+#define CYRILLIC_SMALL_LETTER_ES		0x0441
+#define CYRILLIC_SMALL_LETTER_TE		0x0442
+#define CYRILLIC_SMALL_LETTER_U			0x0443
+#define CYRILLIC_SMALL_LETTER_EF		0x0444
+#define CYRILLIC_SMALL_LETTER_HA		0x0445
+#define CYRILLIC_SMALL_LETTER_TSE		0x0446
+#define CYRILLIC_SMALL_LETTER_CHE		0x0447
+#define CYRILLIC_SMALL_LETTER_SHA		0x0448
+#define CYRILLIC_SMALL_LETTER_SHCHA		0x0449
+#define CYRILLIC_SMALL_LETTER_HARD_SIGN		0x044a
+#define CYRILLIC_SMALL_LETTER_YERU		0x044b
+#define CYRILLIC_SMALL_LETTER_SOFT_SIGN		0x044c
+#define CYRILLIC_SMALL_LETTER_E			0x044d
+#define CYRILLIC_SMALL_LETTER_YU		0x044e
+#define CYRILLIC_SMALL_LETTER_YA		0x044f
 
 /*
  * Режимы УУ
@@ -122,8 +192,7 @@ extern void mmu_setprotection(int idx, t_value word);
  */
 void drum (t_value *sum);
 
-t_stat fprint_sym (FILE *of, t_addr addr, t_value *val,
-	UNIT *uptr, int32 sw);
+void besm6_fprint_cmd (FILE *of, uint32 cmd);
 
 // Unpacked instruction
 typedef struct  {
