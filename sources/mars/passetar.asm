cf27592fc6d9b1d41e244966bd863ba1147bf318
Leo Broukhis <leobru@users.noreply.github.com>
2023-01-14 22:45:10 -0800

    Create passetar.asm

---
+++ sources/mars/passetar.asm
@@ -0,0 +1,14 @@
+          PASSETAR:,NAME,DTRAN  /01.06.84/
+          PASBDI:,SUBP,
+          P/BDSYS:,PC,1024
+          ,ASN,64-6
+          ,ASN,64+6
+          ,ITS,13
+          15,ATX,
+          13,VJM,PASBDI
+          15,XTA,
+          ,STI,13
+          ,UTC,P/BDSYS
+          ,ATX,4
+          13,UJ,
+          ,END,
